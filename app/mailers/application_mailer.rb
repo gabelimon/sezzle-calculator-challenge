ec3151c28efeb6d44df0a8eced23917bbf393de3
Gabe Limon <gabe.limon.1@gmail.com>
2021-04-06 13:56:53 -0700

    Fix actionmailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from=> 'from@example.com'
+  default from=> 'gabe.limon.1@gmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,3 @@
+class UserMailer < ApplicationMailer
+  default from=> 'gabe.limon.1@gmail.com'
+end
